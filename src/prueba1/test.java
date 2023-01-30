127d3c33c9be843599f831098300aa662a44faf2
Jon Ander <jon34tre@gmail.com>
2023-01-30 10:41:50 +0100

    Cambio en la clase principal

--- src/prueba1/test.java
+++ src/prueba1/test.java
@@ -4,6 +4,12 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+
+
 
 	}
 
